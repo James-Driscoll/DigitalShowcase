dab96ed694e094964dae75fee27a9288162b3ea2
James Driscoll <james.e.m.driscoll@gmail.com>
2016-05-18 17:59:57 +0100

    Implemented Carousel for front-page.

--- front-page.php
+++ front-page.php
@@ -11,12 +11,6 @@
 ?>
 
 <?php get_template_parts( array( 'head') ); ?>
-
-<?php global $post; ?>
-<?php
-$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' );
-?>
-
 <?php get_template_parts( array( 'nav') ); ?>
 
 <?php
@@ -24,49 +18,159 @@ $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 560
     $sub_heading = get_field('sub_heading', false, false);
 ?>
 
-<div class="intro" style="background: url(<?php echo $src[0]; ?> ) !important;">
+<div class="intro">
     <div class="container">
-
         <!-- Title -->
         <div class="row title">
             <h1 class="text-right"><?php echo $heading; ?></h1>
             <h2 class="text-right"><?php echo $sub_heading; ?></h2>
         </div>
         <!-- / Title -->
+    </div>
 
-        <!-- Buttons -->
-        <div class="row text-center buttons">
-            <a href="#" class="mybutton btn btn-primary">Read More Case Studies</a>
-            <a href="#" class="mybutton btn btn-primary">Read More Articles</a>
-        </div>
-        <!-- / Buttons -->
+    <!-- Carousel -->
+    <div id="home_carousel" class="carousel slide" data-ride="carousel">
 
-        <!-- Optional Article Overlay -->
-        <?php $article_enabled = get_field('article_enabled', false, false);
-        if ( $article_enabled == 'enabled' ) {
-            $article_title = get_field('article_title', false, false);
-            $article_description = get_field('article_description', false, false);
-            $article_link_text = get_field('article_link_text', false, false);
-            $article_link_url = get_field('article_link_url', false, false); ?>
-            <div class="row">
-                <div class="col-md-7 col-md-offset-5 overlay">
-                    <h3><?php echo $article_title; ?></h3>
-                    <P><?php echo $article_description; ?></p>
-                    <a href="<?php echo get_site_url() . $article_link_url; ?>"><?php echo $article_link_text; ?><i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a>
+        <?php $carousel_slides = get_field('carousel_number_of_slides');
+        $carousel_slide1_image = get_field('carousel_slide1_image');
+        $carousel_slide1_title = get_field('carousel_slide1_title');
+        $carousel_slide1_description = get_field('carousel_slide1_description');
+        $carousel_slide1_link = get_field('carousel_slide1_link');
+        $carousel_slide2_image = get_field('carousel_slide2_image');
+        $carousel_slide2_title = get_field('carousel_slide2_title');
+        $carousel_slide2_description = get_field('carousel_slide2_description');
+        $carousel_slide2_link = get_field('carousel_slide2_link'); ?>
+
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#home_carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#home_carousel" data-slide-to="1"></li>
+            <?php if ( $carousel_slides > 2 ) { ?>
+                <li data-target="#home_carousel" data-slide-to="2"></li>
+                <?php if ( $carousel_slides > 3 ) { ?>
+                    <li data-target="#home_carousel" data-slide-to="3"></li>
+                    <?php if ( $carousel_slides > 4 ) { ?>
+                        <li data-target="#home_carousel" data-slide-to="4"></li>
+                    <?php } ?>
+                <?php } ?>
+            <?php } ?>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="<?php echo $carousel_slide1_image; ?>" alt=""/>
+                <div class="carousel-caption">
+                    <!-- Optional Overlay -->
+                    <div class="row">
+                        <div class="col-md-7 col-md-offset-5 overlay">
+                            <h3 class="text-left"><?php echo $carousel_slide1_title; ?></h3>
+                            <P class="text-left"><?php echo $carousel_slide1_description; ?></p>
+                            <a href="<?php echo $carousel_slide1_link; ?>" class="text-right">Read More<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                    <!-- / Optional Overlay -->
                 </div>
             </div>
-        <?php }; ?>
-        <!-- / Optional Article Overlay -->
-
-        <!-- Scroll Down -->
-        <div class="row">
-            <div class="col-md-12 scroll-down text-center">
-                <a href="#strapline" rel="m_PageScroll2id" class="mPS2id-clicked">
-                    <i class="fa fa-chevron-circle-down" aria-hidden="true"></i>
-                </a>
+            <div class="item">
+                <img src="<?php echo $carousel_slide2_image; ?>" alt=""/>
+                <div class="carousel-caption">
+                    <!-- Optional Article Overlay -->
+                    <div class="row">
+                        <div class="col-md-7 col-md-offset-5 overlay">
+                            <h3 class="text-left"><?php echo $carousel_slide2_title; ?></h3>
+                            <P class="text-left"><?php echo $carousel_slide2_description; ?></p>
+                            <a href="<?php echo $carousel_slide2_link; ?>" class="text-right">Read More<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                    <!-- / Optional Article Overlay -->
+                </div>
             </div>
-        </div>
-        <!-- / Scroll Down -->
+            <?php if ( $carousel_slides > 2 ) {
+                $carousel_slide3_image = get_field('carousel_slide3_image');
+                $carousel_slide3_title = get_field('carousel_slide3_title');
+                $carousel_slide3_description = get_field('carousel_slide3_description');
+                $carousel_slide3_link = get_field('carousel_slide3_link'); ?>
+                <div class="item">
+                    <img src="<?php echo $carousel_slide3_image; ?>" alt=""/>
+                    <div class="carousel-caption">
+                        <!-- Optional Article Overlay -->
+                        <div class="row">
+                            <div class="col-md-7 col-md-offset-5 overlay">
+                                <h3 class="text-left"><?php echo $carousel_slide3_title; ?></h3>
+                                <P class="text-left"><?php echo $carousel_slide3_description; ?></p>
+                                <a href="<?php echo $carousel_slide3_link; ?>" class="text-right">Read More<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a>
+                            </div>
+                        </div>
+                        <!-- / Optional Article Overlay -->
+                    </div>
+                </div>
+                <?php if ( $carousel_slides > 3 ) {
+                    $carousel_slide4_image = get_field('carousel_slide4_image');
+                    $carousel_slide4_title = get_field('carousel_slide4_title');
+                    $carousel_slide4_description = get_field('carousel_slide4_description');
+                    $carousel_slide4_link = get_field('carousel_slide4_link'); ?>
+                    <div class="item">
+                        <img src="<?php echo $carousel_slide4_image; ?>" alt=""/>
+                        <div class="carousel-caption">
+                            <!-- Optional Article Overlay -->
+                            <div class="row">
+                                <div class="col-md-7 col-md-offset-5 overlay">
+                                    <h3 class="text-left"><?php echo $carousel_slide4_title; ?></h3>
+                                    <P class="text-left"><?php echo $carousel_slide4_description; ?></p>
+                                    <a href="<?php echo $carousel_slide4_link; ?>" class="text-right">Read More<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                            <!-- / Optional Article Overlay -->
+                        </div>
+                    </div>
+                    <?php if ( $carousel_slides > 4 ) {
+                        $carousel_slide5_image = get_field('carousel_slide5_image');
+                        $carousel_slide5_title = get_field('carousel_slide5_title');
+                        $carousel_slide5_description = get_field('carousel_slide5_description');
+                        $carousel_slide5_link = get_field('carousel_slide5_link'); ?>
+                        <div class="item">
+                            <img src="<?php echo $carousel_slide5_image; ?>" alt=""/>
+                            <div class="carousel-caption">
+                                <!-- Optional Article Overlay -->
+                                <div class="row">
+                                    <div class="col-md-7 col-md-offset-5 overlay">
+                                        <h3 class="text-left"><?php echo $carousel_slide5_title; ?></h3>
+                                        <P class="text-left"><?php echo $carousel_slide5_description; ?></p>
+                                        <a href="<?php echo $carousel_slide5_link; ?>" class="text-right">Read More<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></a>
+                                    </div>
+                                </div>
+                                <!-- / Optional Article Overlay -->
+                            </div>
+                        </div>
+                    <?php } ?>
+                <?php } ?>
+            <?php } ?>
+    </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#home_carousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#home_carousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    <!-- / Carousel -->
+
+
+        <div class="container">
+            <!-- Scroll Down -->
+            <div class="row">
+                <div class="col-md-12 scroll-down text-center">
+                    <a href="#strapline" rel="m_PageScroll2id" class="mPS2id-clicked">
+                        <i class="fa fa-chevron-circle-down" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </div>
+            <!-- / Scroll Down -->
 
     </div>
 </div>
